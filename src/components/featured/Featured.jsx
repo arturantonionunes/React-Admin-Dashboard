5fff27e5142a9e9140e18364139e3f448766a1ce
Artur Nunes <arturantoniojj@gmail.com>
2022-08-12 22:46:53 -0300

    Criação dos widgets primários

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,52 @@
+import "./featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">To revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={3} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$600</p>
+        <p className="description">
+          Previous transactions processing. Kast payment may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <KeyboardArrowDownIcon />
+              <div className="resultAmount">$30.1k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowDownIcon />
+              <div className="resultAmount">$22.5k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon />
+              <div className="resultAmount">$20.4k</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -17,7 +17,12 @@ const Widget = ({ type }) => {
         title: "USERS",
         isMoney: false,
         link: "See all users",
-        icon: <PersonOutlineIcon className="icon" />,
+        icon: (
+          <PersonOutlineIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2" }}
+          />
+        ),
       };
       break;
     case "order":
@@ -25,7 +30,15 @@ const Widget = ({ type }) => {
         title: "ORDERS",
         isMoney: false,
         link: "View all orders",
-        icon: <ShoppingCartOutlinedIcon className="icon" />,
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2",
+            }}
+          />
+        ),
       };
       break;
     case "earning":
@@ -33,7 +46,12 @@ const Widget = ({ type }) => {
         title: "EARNINGS",
         isMoney: true,
         link: "View net earnings",
-        icon: <MonetizationOnOutlinedIcon className="icon" />,
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2" }}
+          />
+        ),
       };
       break;
     case "balance":
@@ -41,7 +59,12 @@ const Widget = ({ type }) => {
         title: "BALANCES",
         isMoney: true,
         link: "See details",
-        icon: <AccountBalanceWalletOutlinedIcon className="icon" />,
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2" }}
+          />
+        ),
       };
       break;
     default:
